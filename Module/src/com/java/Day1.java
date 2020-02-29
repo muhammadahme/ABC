2d1cc9e55e37d2b2a3b3493514163fed43f64bb0
iqbal <iqbal@DESKTOP-CKDMN9M.home>
2020-02-28 23:40:56 -0500

    my first commit

---
+++ Module/src/com/java/Day1.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+	System.out.println("print my name");
+	System.out.println(1234);
+	System.out.println("i am a boy");
+	System.out.println("you are a girl");
+
+	}
+
+}
